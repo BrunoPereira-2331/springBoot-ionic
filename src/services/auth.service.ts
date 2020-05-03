def8b4e7b58e0a229f927d1398acd126da9c9a31
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-05-03 17:19:47 -0300

    saving user login data in localStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from "@angular/core";
 import { CredentialsDTO } from "../models/credentialsDTO";
 import { HttpClient } from "@angular/common/http";
-import { Config } from "ionic-angular";
 import { API_CONFIG } from "../config/api.config";
+import { LocalUser } from "../models/local_user";
+import { StorageService } from "./storage.service";
 
 @Injectable()
 export class AuthService {
 
-    constructor(public http : HttpClient) {
-
+    constructor(public http : HttpClient, public storage : StorageService) {
     }
 
     authenticate(creds : CredentialsDTO) {
@@ -19,6 +19,19 @@ export class AuthService {
             observe: 'response',
             responseType: 'text'
          });
+    }
+
+
+    successfulLogin(authorizationValue : string) {
+        let tok = authorizationValue.substring(7);
+        let user : LocalUser = {
+            token : tok
+        };
+        this.storage.setLocalUser(user);
+    }
+
+    logout() {
+        this.storage.setLocalUser(null);
     }
 
 }
