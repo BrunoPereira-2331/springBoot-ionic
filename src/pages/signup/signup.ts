5f272af4b44989c4c2c1e9840109d3bce6952e98
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-05-04 20:44:25 -0300

    creating a formGroup to control signup form

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -7,12 +8,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+  formGroup: FormGroup
+  
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+      
+      this.formGroup = this.formBuilder.group({
+        name: ['pedro' , [Validators.required, Validators.minLength(5), Validators.maxLength(150)]],
+        email: ['pedro@gmail.com' , [Validators.required, Validators.email]],
+        type: ['1' , [Validators.required]],
+        cpfOrCnpj: ['06134596280' , [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        password: ['123' , [Validators.required]],
+        logradouro: ['Rua via' , [Validators.required]],
+        number: ['25', [Validators.required]],
+        complement: ['Apto 3' , []],
+        neighborhood: ['Copacabana' , []],
+        postalCode: ['10828333' , [Validators.required]],
+        phone1: ['977261827' , [Validators.required]],
+        phone2: [ , []],
+        phone3: [ , []],
+        stateId: [null , [Validators.required]],
+        cityId: [null , [Validators.required]],
+      });
+    }
+    
+    signupUser() {
+      console.log("passou o form");
+    }
   }
-
-  signupUser() {
-    console.log("passou o form");
-  }
-
-}
+  
