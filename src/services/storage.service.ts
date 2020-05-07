93f4c495853a83f7cc09df968fe93fbb14eb1ebe
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-05-06 21:57:01 -0300

    cart page created

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
 import { LocalUser } from "../models/local_user";
+import { Cart } from "../models/cart";
 
 
 @Injectable()
 export class StorageService {
-
+    
     getLocalUser() : LocalUser {
         let usr = localStorage.getItem(STORAGE_KEYS.localUser);
         if (usr == null) {
@@ -15,7 +16,7 @@ export class StorageService {
             return JSON.parse(usr);
         }
     }
-
+    
     setLocalUser(obj : LocalUser) {
         if (obj == null) {
             localStorage.removeItem(STORAGE_KEYS.localUser);   
@@ -25,4 +26,23 @@ export class StorageService {
         }
     }
 
+    getCart() : Cart {
+        let str = localStorage.getItem(STORAGE_KEYS.cart);
+        if (str != null) {
+            return JSON.parse(str);
+        }
+        else {
+            return null;
+        }
+    }
+    
+    setCart(obj : Cart) {
+        if (obj != null) {
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
+        }
+        else {
+            localStorage.removeItem(STORAGE_KEYS.cart);
+        }
+    }
+    
 }
