e53965d646bdfd852b2564c8df2aa9a890772cf1
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-05-11 19:43:30 -0300

    saving order

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -7,6 +7,7 @@ import { ClientDTO } from '../../models/client.dto';
 import { AddressDTO } from '../../models/address.dto';
 import { ClientService } from '../../services/domain/client.service';
 import { _ParseAST } from '@angular/compiler';
+import { OrderService } from '../../services/domain/order.service';
 
 @IonicPage()
 @Component({
@@ -14,23 +15,24 @@ import { _ParseAST } from '@angular/compiler';
   templateUrl: 'order-confirmation.html',
 })
 export class OrderConfirmationPage {
-
+  
   order: OrderDTO;
   cartItems: CartItem[];
   client: ClientDTO;
   address: AddressDTO;
-
-
+  
+  
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clientService: ClientService) {
-    this.order = this.navParams.get('order');
-  }
-
-  ionViewDidLoad() {
-    this.cartItems = this.cartService.getCart().items;
-    this.clientService.findById(this.order.client.id)
+    public clientService: ClientService,
+    public orderService: OrderService) {
+      this.order = this.navParams.get('order');
+    }
+    
+    ionViewDidLoad() {
+      this.cartItems = this.cartService.getCart().items;
+      this.clientService.findById(this.order.client.id)
       .subscribe(response => {
         this.client = response as ClientDTO;
         this.address = this.findAddress(this.order.deliveryAdress.id, response['addresses']);
@@ -38,15 +40,33 @@ export class OrderConfirmationPage {
       error => {
         this.navCtrl.setRoot('HomePage');
       });
-  }
+    }
+    
+    private findAddress(id : string, list: AddressDTO[]) : AddressDTO {
+      let position = list.findIndex(x => x.id == id);
+      return list[position];
+    }
+    
+    totalValueOrder() {
+      return this.cartService.totalValueCart();
+    }
 
-  private findAddress(id : string, list: AddressDTO[]) : AddressDTO {
-    let position = list.findIndex(x => x.id == id);
-    return list[position];
+    back() {
+      this.navCtrl.setRoot('CartPage')
+    }
+    
+    checkout() {
+      this.orderService.insert(this.order)
+        .subscribe(response => {
+          this.cartService.createOrClearCart();
+          console.log(response.headers.get('location'));
+        },
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
+    }
+    
   }
-
-  totalValueOrder() {
-    return this.cartService.totalValueCart();
-  }
-
-}
+  
